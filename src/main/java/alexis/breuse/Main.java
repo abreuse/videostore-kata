ffb305f054442d2af71893eac913816642031ac0
Alexis <alexis.breuse@gmail.com>
2019-01-19 12:16:18 +0100

    add explicit final variables for price code

--- src/main/java/alexis/breuse/Main.java
+++ src/main/java/alexis/breuse/Main.java
@@ -3,10 +3,5 @@ package alexis.breuse;
 public class Main {
 
     public static void main(String[] args) {
-        Customer customer = new Customer("Alfred");
-        customer.addRental(new Rental(new Movie("The cell", Movie.NEW_RELEASE), 3));
-        customer.addRental(new Rental(new Movie("The cell", Movie.NEW_RELEASE), 3));
-        customer.addRental(new Rental(new Movie("Plan 9 from Outer Space", Movie.REGULAR), 1));
-        customer.statement();
     }
 }
--- src/main/java/alexis/breuse/MovieFactory.java
+++ src/main/java/alexis/breuse/MovieFactory.java
@@ -2,15 +2,19 @@ package alexis.breuse;
 
 public class MovieFactory {
 
+    private final static int PRICE_CODE_REGULAR_MOVIE = 0;
+    private final static int PRICE_CODE_NEW_RELEASE_MOVIE = 1;
+    private final static int PRICE_CODE_CHILDREN_MOVIE = 2;
+
     public static Movie createRegularMovie(String title) {
-        return new Movie(title, 0);
+        return new Movie(title, PRICE_CODE_REGULAR_MOVIE);
     }
 
     public static Movie createNewReleaseMovie(String title) {
-        return new Movie(title, 1);
+        return new Movie(title, PRICE_CODE_NEW_RELEASE_MOVIE);
     }
 
     public static Movie createChildrenMovie(String title) {
-        return new Movie(title, 2);
+        return new Movie(title, PRICE_CODE_CHILDREN_MOVIE);
     }
 }
